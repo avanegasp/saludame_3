565b7c1b2b3262048bae03bd1ec0e2625035fe9a
Angie <arco527@gmail.com>
2019-10-30 16:49:03 -0500

    First commit

---
+++ app.js
@@ -0,0 +1,44 @@
+var express = require('express');
+
+/*
+ * body-parser is a piece of express middleware that
+ *   reads a form's input and stores it as a javascript
+ *   object accessible through `req.body`
+ *
+ * 'body-parser' must be installed (via `npm install --save body-parser`)
+ * For more info see: https://github.com/expressjs/body-parser
+ */
+var bodyParser = require('body-parser');
+
+// create our app
+var app = express();
+
+// instruct the app to use the `bodyParser()` middleware for all routes
+app.use(bodyParser());
+
+// A browser's default method is 'GET', so this
+// is the route that express uses when we visit
+// our site initially.
+app.get('/', function(req, res){
+  // The form's action is '/' and its method is 'POST',
+  // so the `app.post('/', ...` route will receive the
+  // result of our form
+  var html = '<form action="/" method="post">' +
+               '<input type="text" name="userName" />' +
+               '<button type="submit">Enviar</button>' +
+            '</form>';
+
+  res.send(html);
+});
+
+// This route receives the posted form.
+// As explained above, usage of 'body-parser' means
+// that `req.body` will be filled in with the form elements
+app.post('/', function(req, res){
+  var userName = req.body.userName;
+  var html = '<h1>Hola ' + userName +'!' +'</h1>'
+  res.send(html);
+});
+
+
+app.listen(3000, () => console.log('Listening on port 3000'));
